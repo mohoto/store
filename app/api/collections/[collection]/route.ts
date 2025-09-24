815ef495cbc015cf333f2740816a8d926a1de26e
Walid Benmouffok <walidbenmouffok@MacBook-Pro-de-Walid.local>
2025-09-24 20:33:26 +0200

    7 commit

--- app/api/collections/[collection]/route.ts
+++ app/api/collections/[collection]/route.ts
@@ -2,10 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   _req: NextRequest,
-  { params }: { params: { collection: string } }
+  context: { params: Promise<{ collection: string }> }
 ) {
   try {
-    const { collection } = params;
+    const { collection } = await context.params;
     console.log("Fetching products for collection:", collection);
 
     /* const products = await prisma.product.findMany({
